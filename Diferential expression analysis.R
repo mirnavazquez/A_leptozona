a4fa549528c2ffa387ce2d94657a3f797360bdda
Lesem Vasquez <ibq.vsale@yahoo.com>
2021-04-09 17:21:50 -0500

    Importar datos de RSEM

--- Diferential expression analysis.R
+++ Diferential expression analysis.R
@@ -3,3 +3,17 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
 
 BiocManager::install(c("readr","tximport","DESeq2", "vsn",
                        "pheatmap", "apeglm","genefilter"))
+library(tximport)
+list.files("BGen_Results-20210409T213500Z-001/BGen_Results/")
+did <- getwd()
+Ruta <- "C:/Users/GAIA/Documents/RstudioTesis/A_leptozona/BGen_Results-20210409T213500Z-001/BGen_Results/"
+samples <- list.files("BGen_Results-20210409T213500Z-001/BGen_Results/")
+
+files <- file.path(Ruta, samples)
+
+names(files) <-gsub(".genes.results","", samples, perl = T)
+
+txi <- tximport(files, type = "rsem")
+names(txi)
+head(txi$counts)
+save(txi, file="tximport.RData")
