0d04f83614e610c55699b5f09c4dac7d630acc57
Lesem Vasquez <ibq.vsale@yahoo.com>
2021-04-19 16:12:01 -0500

    Genesunicos importados de excel

---
+++ Ejercicio1_PracticaDiferentialeXpressionAnalysis.R
@@ -0,0 +1,75 @@
+# Instalar librerias  -----------------------------------------------------####
+if (!requireNamespace("BiocManager", quietly = TRUE)) 
+  install.packages("BiocManager")
+
+BiocManager::install(c("readr","tximport","DESeq2", "vsn",
+                       "pheatmap", "apeglm","genefilter"))
+# Llamar a las librerias --------------------------------------------------####
+library(tximport)
+library(DESeq2)
+library(tidyverse)
+library(readxl)
+library(janitor)
+library(ggplot2)
+# Importar datos ----------------------------------------------------------####
+list.files("BGen_Results-20210409T213500Z-001/BGen_Results/")
+did <- getwd()
+Ruta <- "C:/Users/GAIA/Documents/RstudioTesis/A_leptozona/BGen_Results-20210409T213500Z-001/BGen_Results/"
+samples <- list.files("BGen_Results-20210409T213500Z-001/BGen_Results/")
+files <- file.path(Ruta, samples)
+names(files) <-gsub(".genes.results","", samples, perl = T)
+txi <- tximport(files, type = "rsem")
+names(txi)
+head(txi$counts)
+#save(txi, file="tximport.RData")
+# Crear samples described -------------------------------------------------####
+samples<-as_tibble(samples)
+samples<-read_excel("CuadroSuplementario1_Resultasdos -MVRL.xlsx", 
+                    sheet = "Sheet1") %>%
+  clean_names() %>%
+  select(-c(secuncias_crudas, x5)) %>%
+  filter(x4 != "Librerías") %>%
+  fill(sexo) %>%
+  fill(dias) %>%
+  mutate(dias = str_replace(dias, " \\(.*", "")) %>%
+  mutate(color = case_when(
+    dias == "02-03" ~ "No_color",
+    dias == "03-05" ~ "Transicion_a_verde",
+    dias == "10-12" ~ "Coloracion_verdosa"
+  )) %>%
+  mutate(x4 = str_replace(x4, "$", ".genes.results")) %>%
+  rename(Name = x4) %>%
+  select(color, dias, sexo, Name) %>%
+  mutate(dias = str_replace(dias, "-", "_"))
+
+# Leer el archivo txi -----------------------------------------------------####
+load("tximport.RData")
+# Transformar -------------------------------------------------------------####
+###Transformar los 0 en 1, siguiendo: https://support.bioconductor.org/p/92763/
+which(txi$length == 0)
+zero <- which(txi$length == 0)
+txi$length[zero] <- 1
+which(txi$length == 0)
+# Core analysis -----------------------------------------------------------####
+dds <- DESeqDataSetFromTximport(txi, samples, ~1)
+colData(dds)
+keep = rowSums(counts(dds)) >= 10
+dds = dds[keep, ]
+
+#dds$color <- relevel(dds$color,"No_color")
+dds$group <- factor(paste0(dds$color, dds$dias, dds$sexo))
+design(dds) <- ~ group
+#dds <- DESeq(dds)
+#save(dds, file="dds.RData")
+load("dds.RData")
+plotDispEsts(dds)
+
+vsd <- vst(dds, blind=FALSE)
+pcaData <- plotPCA(vsd, intgroup=c("color", "dias", "sexo"), returnData=TRUE)
+percentVar <- round(100 * attr(pcaData, "percentVar"))
+ggplot(pcaData, aes(PC1, PC2, color=color, shape=sexo)) +
+  geom_point(size=3) +
+  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+  ylab(paste0("PC2: ",percentVar[2],"% variance")) + 
+  coord_fixed()
+
---
+++ UniqMalesGenes.R
@@ -0,0 +1,47 @@
+#indicar el directorio de trabajo
+setwd("C:\\Users\\GAIA\\Documents\\RstudioTesis\\A_Leptozona")
+
+# cargar paquete readx
+library(readxl)
+
+# buscar la ruta del archivo de excel
+file.choose()
+
+# Copiar ruta de la consola y guardar en variable
+ruta_excel <- "C:\\Users\\GAIA\\Desktop\\AlesDEGDiciembre20.xlsx"
+
+# como mirar las hojas de tu excel
+excel_sheets(ruta_excel)
+
+#####################################
+# 2. importar excel con código de R #
+#####################################
+
+# importar datos de los ID para revisar cuales son los datos repetitivos
+genesunicos <- read_excel(ruta_excel,
+                         sheet = "MaConcat",
+                         range = "F1:F993")
+#unique nos permite analizar las veces que se repite un dato y te arroja solo 
+#los datos que se encuentran una sola ves en formato de tabla "Males"
+
+unique(genesunicos)
+
+#Nombramos la tabla para darle salida 
+Genes_Males <- unique(genesunicos)
+
+#en este caso repetimos la accion pero cambiando los datos por datos "Females"
+genesunicos2 <- read_excel(ruta_excel,
+                           sheet ="FeConcat",
+                           range = "e1:e10191")
+
+#volvemos a llamar a unique para el analisis de los datos 
+unique(genesunicos2)
+
+#Nombramos nuevamente los datos de salida
+Genes_Females <- unique(genesunicos2)
+
+#Para generar otra tabla con los datos repetidos utilizamos la funcion  
+
+duplicated(genesunicos)
+
+
